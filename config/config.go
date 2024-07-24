935ae614a2540fef82cf936d17c7c63483a9b003
Aykhan Shahsuvarov <aykhan.shahs@gmail.com>
2024-07-25 00:38:35 +0400

    🔖 bump version to 0.2.0

--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ import (
 )
 
 const (
-	VERSION                 = "0.1.0"
+	VERSION                 = "0.2.0"
 	DefaultUserAgent        = "Dodo/" + VERSION
 	ProxyCheckURL           = "https://www.google.com"
 	DefaultMethod           = "GET"
