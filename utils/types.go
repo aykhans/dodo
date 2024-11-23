ede74c17b2348e5e72ce14a06e30c02b3609de64
Aykhan Shahsuvarov <aykhan.shahs@gmail.com>
2024-11-23 16:13:17 +0400

    📚 Add docs

--- utils/types.go
+++ utils/types.go
@@ -5,15 +5,19 @@ import (
 	"errors"
 )
 
+// Don't call this struct directly, use NewOption[T] or NewNoneOption[T] instead.
 type Option[T any] struct {
+	// value holds the actual value of the Option if it is not None.
 	value T
-	none  bool
+	// none indicates whether the Option is None (i.e., has no value).
+	none bool
 }
 
 func (o *Option[T]) IsNone() bool {
 	return o.none
 }
 
+// The returned value can be nil, if the Option is None, it will return nil and an error.
 func (o *Option[T]) ValueOrErr() (*T, error) {
 	if o.IsNone() {
 		return nil, errors.New("Option is None")
@@ -21,6 +25,7 @@ func (o *Option[T]) ValueOrErr() (*T, error) {
 	return &o.value, nil
 }
 
+// The returned value can't be nil, if the Option is None, it will return the default value.
 func (o *Option[T]) ValueOr(def *T) *T {
 	if o.IsNone() {
 		return def
@@ -28,6 +33,7 @@ func (o *Option[T]) ValueOr(def *T) *T {
 	return &o.value
 }
 
+// The returned value can't be nil, if the Option is None, it will panic.
 func (o *Option[T]) ValueOrPanic() *T {
 	if o.IsNone() {
 		panic("Option is None")
