ed52fff3631b97eee266c4f1d1bd0bd8a57c34ff
Aykhan Shahsuvarov <aykhan.shahs@gmail.com>
2025-03-20 16:31:39 +0400

    ⬆️ bump version to 0.6.1

--- config/config.go
+++ config/config.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	VERSION             string        = "0.6.0"
+	VERSION             string        = "0.6.1"
 	DefaultUserAgent    string        = "Dodo/" + VERSION
 	DefaultMethod       string        = "GET"
 	DefaultTimeout      time.Duration = time.Second * 10
