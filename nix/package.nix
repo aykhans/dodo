a85c82e75133591d56f2115ec3b8208dd351eb55
Aykhan Shahsuvarov <aykhan.shahs@gmail.com>
2026-08-25 10:28:29 +0400

    fix(nix): update vendorHash for bubbles 2.2.0

--- nix/package.nix
+++ nix/package.nix
@@ -11,7 +11,7 @@
 
   src = lib.cleanSource ../.;
 
-  vendorHash = "sha256-8YAm/zjbF+vLUXPaROwC7c/DDfFGknnHhryFgkCVGl4=";
+  vendorHash = "sha256-bxIJoAsrzXMeTyjIGKL0GgVddJFpieuMRsDihWw+EkM=";
 
   subPackages = [ "cmd/cli" ];
 
